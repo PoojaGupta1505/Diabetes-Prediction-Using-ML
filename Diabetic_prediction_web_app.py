335f2f3fccd70f3432bf8c77b44caebe976d292b
Pooja Gupta <130153246+PoojaGupta1505@users.noreply.github.com>
2024-01-04 15:58:57 +0530

    Update Diabetic_prediction_web_app.py

--- Diabetic_prediction_web_app.py
+++ Diabetic_prediction_web_app.py
@@ -11,7 +11,7 @@ import streamlit as st
 
 
 # Loading the saved model
-loaded_model = pickle.load(open('trained_model (1).sav', 'rb'))
+loaded_model = pickle.load(open("trained_model (1).sav", 'rb'))
 
 # creating a function for Prediction
 
